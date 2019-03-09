f7e747f83c23f99f3a994ef5ca7b908a9ec33dc4
melasafitri <mellauser68@gmail.com>
2019-03-09 20:31:13 +0700

    Episode-22

---
+++ resources/views/admin/pages/add.blade.php
@@ -0,0 +1,22 @@
+@extends('admin.main')
+@section('title','Kategori Tambah')
+@section('content')
+<h1>Kategori <small class="text-muted">Tambah</small></h1>
+<hr>
+
+<div class="row">
+	<div class="col-md-6 mb-3">
+		<form method="POST" action="{{ route('admin.kategori.add') }}">
+			{{ csrf_field() }}
+			<div class="card">
+				
+				<div class="card-header">
+					<h5>buat Kategori Baru</h5>
+				</div><!-- End Card Header-->
+
+			</div><!-- End Card-->
+		</form>
+	</div>
+</div>
+
+@endsection
